b2800db5a3a723f86b653acd27afa6ec71e8f6ce
Tiago Rodrigues <isatiago098@gmail.com>
2022-05-26 19:52:07 -0300

    Resolucao exercicio nome

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt('Qual é o seu nome ? ');
+let sobrenome = prompt('Digite seu sobrenome ');
+
+alert(`Nome completo; ${nome.toUpperCase()} ${sobrenome}`);
+alert(`Nome catalogo; ${sobrenome.toUpperCase()} ${nome}`);
